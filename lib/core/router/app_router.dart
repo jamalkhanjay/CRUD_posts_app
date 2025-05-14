0a6c81c61844a6ff73916c0b204489e7829eb956
jamalkhanjay <jamalkhanwaxir@gmail.com>
2025-05-14 23:33:48 +0500

    minor fixes

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,7 +1,6 @@
 import 'package:clean_architecture_posts_project/features/posts/data/models/post.dart';
 import 'package:clean_architecture_posts_project/features/posts/presentation/pages/add_edit_post_page.dart';
 import 'package:clean_architecture_posts_project/features/posts/presentation/pages/posts_page.dart';
-// import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter appRouter = GoRouter(
